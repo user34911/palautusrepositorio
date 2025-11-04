7ced07d9d7b039d98f1aedcc2d472015c609a324
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-11-04 21:51:12 +0200

    Tehtävä 5

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,29 +1,64 @@
 import requests
 from player import Player
+from rich.console import Console
+from rich.table import Table
+from rich import print as rprint
 
-def players_by_nationality(players: list, nation: str):
-    suitable = []
+class PlayerReader:
+    def __init__(self, url: str):
+        response = requests.get(url).json()
+        self.players = []
+
+        for player_dict in response:
+            player = Player(player_dict)
+            self.players.append(player)
+
+    def __iter__(self):
+        for player in self.players:
+            yield player
+
+class PlayerStats:
+    def __init__(self, players):
+        self.players = players
+
+    def top_scorers_by_nationality(self, nation):
+        suitable = []
+        for player in self.players:
+            if player.nationality == nation:
+                suitable.append(player)
+        suitable = sorted(suitable, key=lambda player: player.goals+player.assists, reverse=True)
+        return suitable
+
+def make_table(players, season, nationality):
+    table = Table(title=f"Season {season} players from {nationality}")
+
+    table.add_column("Released", style="cyan")
+    table.add_column("teams", style="magenta")
+    table.add_column("goals", justify="right", style="green")
+    table.add_column("assists", justify="right", style="green")
+    table.add_column("points", justify="right", style="green")
+
     for player in players:
-        if player.nationality == nation:
-            suitable.append(player)
-    return suitable
+        table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.goals+player.assists))
+
+    return table
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
+    console = Console()
+    season = console.input("season [magenta](2018-19, 2019-20, 2020-21, 2021-22, 2022-23, 2023-24, 2024-25, 2025-26)[/magenta]: ")
+    url = "https://studies.cs.helsinki.fi/nhlstats/"+season+"/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    nation = "FIN"
-    filtered_players = players_by_nationality(players, nation)
-    sorted_players = sorted(filtered_players, key=lambda player: player.goals+player.assists, reverse=True)
-    print(f"Players from: {nation}")
-
-    for player in sorted_players:
-        print(player)
+    #url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    #reader = PlayerReader(url)
+    #stats = PlayerStats(reader)
+    while True:
+        nationality = console.input("nationality [magenta](USA, FIN, CAN, SWE CZE, RUS, SLO, FRA, GBR, SVK, DEN, NED, AUT, BLR, GER, SUI, NOR, UZB, LAT, AUS)[/magenta]: ")
+        if nationality == "exit":
+            break
+        players = stats.top_scorers_by_nationality(nationality)
+        table = make_table(players, season, nationality)
+        console.print(table)
 
 main()
