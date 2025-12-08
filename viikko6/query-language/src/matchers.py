9d953414980c6d4d78e2f4c8984a7d2529521c2a
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-12-08 19:31:26 +0200

    vko6 t5

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -64,3 +64,19 @@ class Or:
                 return True
 
         return False
+
+class QueryBuilder:
+    def __init__(self, query = All()):
+        self.query = query
+
+    def plays_in(self, team):
+        return QueryBuilder(And(self.query, PlaysIn(team)))
+
+    def has_at_least(self, value, attr):
+        return QueryBuilder(And(self.query, HasAtLeast(value, attr)))
+
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(And(self.query, HasFewerThan(value, attr)))
+
+    def build(self):
+        return self.query
