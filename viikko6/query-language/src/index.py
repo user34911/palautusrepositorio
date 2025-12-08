dda353891bc087308fc6b4ba03f84edcf304132d
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-12-08 19:19:37 +0200

    vko 6 t4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
@@ -8,9 +8,14 @@ def main():
     stats = Statistics(reader)
 
     matcher = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("COL"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
     )
+
 
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,14 +9,12 @@ class And:
 
         return True
 
-
 class PlaysIn:
     def __init__(self, team):
         self._team = team
 
     def test(self, player):
         return player.team == self._team
-
 
 class HasAtLeast:
     def __init__(self, value, attr):
@@ -28,14 +26,12 @@ class HasAtLeast:
 
         return player_value >= self._value
 
-
 class All:
     def __init__(self):
         pass
 
     def test(self, player):
         return True
-
 
 class Not:
     def __init__(self, *matchers):
@@ -48,7 +44,6 @@ class Not:
 
         return True
 
-
 class HasFewerThan:
     def __init__(self, value, attr):
         self._value = value
@@ -58,3 +53,14 @@ class HasFewerThan:
         player_value = getattr(player, self._attr)
 
         return player_value < self._value
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
