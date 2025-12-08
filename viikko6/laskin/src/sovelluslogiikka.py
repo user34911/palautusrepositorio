f0ac780cb3b38860050564c135e871134b699246
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-12-08 18:14:11 +0200

    vko 6 t1

---
+++ viikko6/laskin/src/sovelluslogiikka.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+class Sovelluslogiikka:
+    def __init__(self, arvo=0):
+        self._arvo = arvo
+
+    def miinus(self, operandi):
+        self._arvo = self._arvo - operandi
+
+    def plus(self, operandi):
+        self._arvo = self._arvo + operandi
+
+    def nollaa(self):
+        self._arvo = 0
+
+    def aseta_arvo(self, arvo):
+        self._arvo = arvo
+
+    def arvo(self):
+        return self._arvo
+
+class BinaariOperaatio(ABC):
+    def __init__(self, logiikka, io):
+        self.io = io
+        self.sovelluslogiika = logiikka
+
+    def suorita(self):
+        self.laske()
+
+    @abstractmethod
+    def laske(self):
+        pass
+
+class Summa(BinaariOperaatio):
+    def __init__(self, logiikka, io):
+        super().__init__(logiikka, io)
+
+    def laske(self):
+        return self.sovelluslogiika.plus(int(self.io()))
+
+class Erotus(BinaariOperaatio):
+    def __init__(self, logiikka, io):
+        super().__init__(logiikka, io)
+
+    def laske(self):
+        return self.sovelluslogiika.miinus(int(self.io()))
+
+class Nollaus(BinaariOperaatio):
+    def __init__(self, logiikka, io):
+        super().__init__(logiikka, io)
+
+    def laske(self):
+        return self.sovelluslogiika.nollaa()
